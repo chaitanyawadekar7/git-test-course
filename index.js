6ba1af4f1974af439ed9e5fab3d1f7195ab1eeb1
Chaitanya Wadekar <chaitanyawadekar6@gmail.com>
2026-06-19 10:45:33 +0530

    two new lines added in index file

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ console.log('This is your first repostery');
 console.log('I am learning the course');
 
 console.log ('new line added in index file');
+
+console.log('Index js got new feature');
+console.log('if you do everyday small things then oneday it will be strong');
