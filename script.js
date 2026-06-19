6d09e06978939cd7da011454e2d38d69b521d72c
Chaitanya Wadekar <chaitanyawadekar6@gmail.com>
2026-06-19 10:19:14 +0530

    newscript added in script file

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function newFeatureadd(){
     console.log('Added new function in branch');
     console.log('new feature added');
 }
+
+function newScript() {
+    console.log('New script added');
+    console.log('NEW Function added');
+}
